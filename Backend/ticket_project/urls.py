f0b79e281e4938a0f7e6049a3a9bcdd875db758f
Justin Harris <justin.r.harris@gmail.com>
2022-12-17 12:21:36 -0500

    events and venues added

--- Backend/ticket_project/urls.py
+++ Backend/ticket_project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('ticket_app.urls')),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
