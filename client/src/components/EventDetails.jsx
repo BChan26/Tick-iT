578aeaeaca9e31d914abcfb6f567c6b11c1f6b3f
Justin Harris <justin.r.harris@gmail.com>
2022-12-19 15:13:09 -0500

    event details pages are working

--- client/src/components/EventDetails.jsx
+++ client/src/components/EventDetails.jsx
@@ -1,9 +1,33 @@
-export default function EventDetails () {
+import { useEffect, useState, useContext } from "react"
+import { useParams } from "react-router-dom"
+import { DataContext } from "../DataContext"
+import axios from "axios"
 
-    
-    return (
-        <div>
+function EventDetails() {
+  let { id } = useParams()
 
-        </div>
-    )
+  //   const { venues } = useContext(DataContext)
+  const [event, setEvent] = useState(null)
+
+  useEffect(() => {
+    const getEvent = async () => {
+      const response = await axios.get(`http://localhost:8000/events/${id}`)
+
+      setEvent(response.data)
+    }
+    getEvent()
+  }, [])
+
+  return event ? (
+    <div className="detail">
+      <h1>{event.eventname}</h1>
+      <h2>{event.datetime}</h2>
+      <h2>{event.price}</h2>
+      <h2>{event.details}</h2>
+    </div>
+  ) : (
+    <h1> event not found</h1>
+  )
 }
+
+export default EventDetails
