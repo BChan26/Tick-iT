0923e1a6a08dc26780a05c3c2a7211bbc54042e2
Justin Harris <justin.r.harris@gmail.com>
2022-12-19 10:58:48 -0500

    added Axios call to map out venues

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap"
+        rel="stylesheet"
+      />
+      <h1 className="Main">Tick-It</h1>
+    </div>
+  )
+}
