dacc4ea93f71873aed071da02370f4e5a99f6e66
Brandon Chan <bchan26@bu.edu>
2022-12-19 10:01:44 -0500

    after installing axios and react router dom, we edited the index.js file to import browserrouter, we edited the app.js to include our routes, then created components for Home, Venues, then EventList to then connect to EventDetails. We have documentation from Brittany to create forms in Django, to import into our front end to make the axios calls

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Venues from './Venues'
+
+export default function Home () {
+
+
+    return (
+    <div id='HomeContent'>
+        <header>
+            <Venues></Venues>
+        </header>
+    </div>
+    )}
